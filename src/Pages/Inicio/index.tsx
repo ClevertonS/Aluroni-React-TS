28894685d45a5709dd05898fe00f847452cb5c6d
Cleverton Teixeira <clevertons.b.t@gmail.com>
2022-05-09 13:09:45 -0300

    Project Aluroni Started

---
+++ src/Pages/Inicio/index.tsx
@@ -0,0 +1,10 @@
+import cardapio from 'data/cardapio.json';
+
+export function Inicio() {
+  return (
+    <section>
+      <h3>Recomendações da cozinha</h3>
+      <div></div>
+    </section>
+  );
+}
